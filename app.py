a4ab90f24925ebf713ef1117158966410466f2f2
Tianyi Wei <tywei08@users.noreply.huggingface.co>
2024-04-11 00:44:55 +0000

    Update app.py

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ with gr.Blocks(css=css_styles) as demo:
     
     play_button.click(fn=play_music, inputs=[], outputs=[background_music, background_music])
 
-    clear_button.click(fn=reset, inputs=[], outputs=[mood, sweetness, sour, savory, bitter, flavor_association, drinking_experience, soberness_level, allergies, additional_requests], output_recipe, play_button, background_music)
+    clear_button.click(fn=reset, inputs=[], outputs=[mood, sweetness, sour, savory, bitter, flavor_association, drinking_experience, soberness_level, allergies, additional_requests, output_recipe, play_button, background_music])
         
 if __name__ == "__main__":
     demo.launch(#enable_queue=False,
