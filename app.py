c449d29cb20540be9bf0e0b0e7dc085df24e6d2a
Tianyi Wei <tywei08@users.noreply.huggingface.co>
2024-04-11 01:04:32 +0000

    Update app.py

--- app.py
+++ app.py
@@ -131,7 +131,7 @@ with gr.Blocks(css=css_styles) as demo:
     
     play_button.click(fn=play_music, inputs=[], outputs=[background_music, background_music])
 
-    save_pdf_button.click(fn=save_as_pdf, inputs=output_recipe, outputs=[]])
+    save_pdf_button.click(fn=save_as_pdf, inputs=output_recipe, outputs=[])
     
     clear_button.click(fn=reset, inputs=[], outputs=[mood, sweetness, sour, savory, bitter, flavor_association, drinking_experience, soberness_level, allergies, additional_requests, output_recipe, play_button, background_music, save_pdf_button])
         
